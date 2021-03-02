f5b6aca86bfd3152ae30bba9dcf70bdd9cef9e06
eric_boise <boiseeric@gmail.com>
2021-03-02 11:49:42 -0800

    add default sensor effects json

--- ColorCameraSensor.cs
+++ ColorCameraSensor.cs
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 LG Electronics, Inc.
+ * Copyright (c) 2019-2021 LG Electronics, Inc.
  *
  * This software contains code licensed as described in LICENSE.
  *
@@ -9,11 +9,12 @@ namespace Simulator.Sensors
 {
     using System.Linq;
     using Simulator.Bridge.Data;
+    using Simulator.Sensors.Postprocessing;
     using Simulator.Utilities;
-    using UnityEngine;
     using UnityEngine.Rendering.HighDefinition;
 
     [SensorType("Color Camera", new[] { typeof(ImageData) })]
+    [DefaultPostprocessing(typeof(Rain), typeof(SunFlare))]
     public class ColorCameraSensor : CameraSensorBase
     {
         // fix for 1000+ ms lag spike that appears approx. 1 s after starting
